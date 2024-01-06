08b603355de22b674245bca531ed17b99a67ad45
Akarshan Mishra <93630550+1pharaxh@users.noreply.github.com>
2024-01-06 13:59:41 -0700

    refactor: changed pdf-read

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 // IMPORTANT: THIS IS HOW YOU MAKE AN API : https://www.youtube.com/watch?v=O-NGENb6LNg
+const url: string =
+  "https://firebasestorage.googleapis.com/v0/b/hacked-2024-bucket.appspot.com/o/files%2Fresume_akarshan_mishra.pdf?alt=media&token=40b03f6c-16d1-4d4e-94e1-42dff456c9c9";
+
 export const GET = async (req: Request, res: Response) => {
   return NextResponse.json({ message: "Hello from the API!" });
 };
